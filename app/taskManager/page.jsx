9c0b8fdb4a00f24ab5ea973f218446a8bd667ff7
Dorcas Bamisile <bamisiledorcas@gmail.com>
2023-10-02 11:16:14 +0100

    Added authentication to taskManager

--- app/taskManager/page.jsx
+++ app/taskManager/page.jsx
@@ -2,16 +2,22 @@
 
 import React, { useState } from "react";
 import TaskList from "../components/taskManager/TaskList";
+import { userAuth } from "../components/context/AuthContext";
 
 const page = () => {
   const [taskFormVisible, setTaskFormVisible] = useState(false);
+  const { user } = userAuth();
   return (
     <div>
-      <h1>Task Manager</h1>
-      <TaskList
-        taskFormVisible={taskFormVisible}
-        setTaskFormVisible={setTaskFormVisible}
-      />
+      <h1>Manage Your Tasks</h1>
+      {user ? (
+        <TaskList
+          taskFormVisible={taskFormVisible}
+          setTaskFormVisible={setTaskFormVisible}
+        />
+      ) : (
+        <p>Please sign in to manage your tasks</p>
+      )}
     </div>
   );
 };
